9157c71c878ef749ede59beb8c8465ae8068b644
vaish <vaish@VAISHNAVY>
2024-10-02 22:37:21 +0530

    Add project files.

---
+++ Web_Authentication/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace Web_Authentication
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "aa" && TextBox2.Text == "bb")
+            {
+                FormsAuthentication.RedirectFromLoginPage(TextBox1.Text, true);
+                Response.Redirect("WebForm1.aspx");
+            }
+            else
+            {
+                Label1.Text = "Invalid";
+            }
+        }
+    }
+}
+    
+            
+
+    
